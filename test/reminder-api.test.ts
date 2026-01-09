d2d56001a949e3f98d98be8e8151e5f9c13cc4d4
Daniel17971 <danielisa1922@gmail.com>
2026-01-09 21:06:28 +0000

    Initial commit

---
+++ test/reminder-api.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib/core';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as ReminderApi from '../lib/reminder-api-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/reminder-api-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new ReminderApi.ReminderApiStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
